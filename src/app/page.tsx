c641d6b3a03d5790fb103d852fd11d5803a5cc4a
Aqsa-Memon <muhammadzaidjati2@gmail.com>
2024-10-27 20:19:53 +0500

    My-portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
 import Header from "./components/header";
 import Hero from "./components/hero";
 import Footer from "./components/footer";
+import About from "./about/page";
+import Contact from "./contact/page";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Hero />
+      <br />
+      <br />
+      <About />
+      <br />
+      <br />
+      <Contact />
+      
       <Footer />
     </div>
   )}
